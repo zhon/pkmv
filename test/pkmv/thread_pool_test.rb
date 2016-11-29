d32bdfbf06524cb1648b9cc2e8abd7a9adb3a487
zhon <zhon@xputah.org>
2016-11-29 15:19:32 -0700

    test for executing in parallel

--- test/pkmv/thread_pool_test.rb
+++ test/pkmv/thread_pool_test.rb
@@ -45,12 +45,43 @@ describe ThreadPool do
   it 'executes 2 items pushed' do
     x,y = 0,''
     tp = ThreadPool.new(2)
-    tp.process
     tp.push -> { y << 'hello' }
     tp.push -> { x += 1 }
+    tp.process
     tp.finish
     x.must_equal 1
     y.must_equal 'hello'
+  end
+
+  it 'will execute in parallel' do
+    tp = ThreadPool.new(4)
+    t = Time.now
+    tp.push -> { sleep 1.0/1000 }
+    tp.push -> { sleep 1.0/1000 }
+    tp.push -> { sleep 1.0/1000 }
+    tp.push -> { sleep 1.0/1000 }
+    tp.finish
+    tp.process
+    (Time.now - t  < 2.0/1000).must_equal TRUE
+  end
+
+  ############ Property based TDD
+
+  it 'with 0 threads all items complete' do
+    skip
+    test_q = Queue.new
+    tp = ThreadPool.new(0)
+    5.times do
+      tp.push -> { test_q << 1 }
+    end
+    tp.process
+    test_q.length.must_equal 5
+  end
+
+  it 'with mutiple threads all items run in parallel' do
+  end
+
+  it 'with mutiple threads all items complete' do
   end
 
 end
